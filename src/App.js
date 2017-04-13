c6e83c365eb13633ecfe972aba7d4308ca2564b6
William Croke <williamcroke@shadowgames-dev.com>
2017-04-13 10:26:33 +0100

    Finalised application contact and information page, addition of README!

--- src/App.js
+++ src/App.js
@@ -15,6 +15,18 @@ var Info = React.createClass({
   render: function() {
     return (
       <div className="App">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1 text-center">
+            <h1 className="lgHd">Application Information</h1>
+            <div className="lgTxt">
+            <p>This application was built for the purpose of development for a college module!<br/>
+            This application does not store any private infromatoin, any information submitted through this application 
+            is immediatly destroyed!</p>
+            <p>If you have any questions or queries please do not hesitate to contact us using any of the information in the 
+            footer of this application on all pages or through the additional contact information on our Contact page</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   } 
--- src/Contact.js
+++ src/Contact.js
@@ -1,39 +1,36 @@
 import React from 'react';
 import './App.css';
-import nodemailer from 'nodemailer';
+import AlertContainer from 'react-alert';
+
+var msg;
 
 var Contact = React.createClass({
+
+    validate: function (mail) {
+        var email = this.refs.userEmail.value;
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(email)){
+        this.sendMail(mail);
+    }else{
+        msg.error('Please ensure you have entered a valid email address!', { time: 2000});
+    }
+  },
 
 	sendMail: function(mail){
 		var email = this.refs.userEmail.value;
 		var message = this.refs.userMessage.value;
 
-		this.post('/contact', function(req,res,next){
-    		var to = req.body.email;
-    		var transporter = nodemailer.createTransport({
-        	service:'Gmail',
-        	auth:{
-         	   user:'croke.college.testing@gmail.com',
-         	   pass:'Crokes321'
-        	}
-    	});
+        if(message !== ""){
+		msg.show('Your message has been sent, Thank you!', { time: 3000});
 
-        var mailOptions = {
-    		from: email,
-    		to: 'info@shadowgames-dev.com.com',
-    		subject: 'Crokes Chipper Info Request',
-    		text: message 
-		};
+        console.log("Send Mail to :" + email);
+        console.log("Message: " + message);
 
-		transporter.sendMail(mailOptions, function(error, info){
-    		if(error){
-        		console.log(error);
-    		}else{
-        		console.log('Message sent: ' + info.response);
-    	};
-		});
-
-		});
+        this.refs.userEmail.value = "";
+        this.refs.userMessage.value = "";
+    }else{
+        msg.error('Do not leave any form areas blank!', { time: 2000});
+    }
     
 
 	},
@@ -41,25 +38,32 @@ var Contact = React.createClass({
   render: function() {
     return (
       <div className="App">
+      <AlertContainer ref={(a) => msg = a} {...this.alertOptions} />
         <div className="contactForm">
                 <div className="row">
                     <div className="col-xs-4 col-xs-offset-1 text-center">
                     <h1>Contact Us</h1>
                         <form>
                             <div className="form-group ">
-                                <small className="form-text text-muted">Your Email -</small>
-                                <input ref="userEmail" type="text" className="form-control form-control-success"/>
+                                <p className="form-text text-muted">Your Email -</p>
+                                <input placeholder="me@example.com" ref="userEmail" type="text" className="form-control form-control-success"/>
                             </div>
                             <div className="input-field">
-                                <small className="form-text text-muted">Your message -</small>
-                                <textarea ref="userMessage" className="form-control" name="emailcontent"/><br/>
+                                <p className="form-text text-muted">Your message -</p>
+                                <textarea placeholder="Your message..." ref="userMessage" className="form-control" name="emailcontent"/><br/>
                             </div>
-                            <button type="button" className="btn btn-primary" onClick="{this.sendMail}">Send message</button>
+                            <button type="button" className="btn btn-primary" onClick={this.validate}>Send message</button>
                         </form>
                     </div>
                     <div className="col-xs-4 col-xs-offset-1 text-center">
                     <h1>Additional Contact Info</h1>
-                    	<p>Some random contact information!!!</p>
+                    <div className="mdTxt">
+                    	<p>Currently this form does not collect any data or send any appropriate emails/messages.<br/>
+                        If you experienced any issues while using this applcation please do not hesitate to contact us 
+                        using the following information: </p>
+                        <p><b><i>Telephone - </i></b><a href="tel:+353857420161">+353 (85) 742 0161</a></p>
+                        <p><b><i>Email - </i></b><a href="mailto:info@shadowgames-dev.com">info@shadowgames-dev.com</a></p>
+                    </div>
                     </div>
                     </div>
                 </div>
